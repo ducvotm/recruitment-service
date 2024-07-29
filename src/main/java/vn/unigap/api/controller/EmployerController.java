d38d067d483f087d102f9676cf17589368b8c932
duke141 <vtminhduc140101@gmail.com>
2024-07-29 09:33:30 +0700

    Delete path /create & adding logic for checking existed email

--- src/main/java/vn/unigap/api/controller/EmployerController.java
+++ src/main/java/vn/unigap/api/controller/EmployerController.java
@@ -3,32 +3,49 @@ package vn.unigap.api.controller;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import vn.unigap.api.common.ApiException;
 import vn.unigap.api.dto.in.EmployerDtoIn;
 import vn.unigap.api.dto.out.ApiResponse;
 import vn.unigap.api.dto.out.EmployerDtoOut;
-import vn.unigap.api.service.EmployerServiceImpl;
+import vn.unigap.api.service.EmployerService;
+
 
 @RestController
 @RequestMapping("/employer")
 public class EmployerController {
 
-    private final EmployerServiceImpl employerServiceImpl;
+    private final EmployerService employerService;
 
-    public EmployerController(EmployerServiceImpl employerServiceImpl) {
-        this.employerServiceImpl = employerServiceImpl;
+    public EmployerController(EmployerService employerService) {
+        this.employerService = employerService;
     }
 
     /*Create employer*/
-    @PostMapping("/create")
+    @PostMapping()
     public ResponseEntity<ApiResponse<EmployerDtoOut>> createEmployer(@RequestBody EmployerDtoIn employerDtoIn) {
-        EmployerDtoOut createdEmployer = employerServiceImpl.create(employerDtoIn);
+        try {
+            EmployerDtoOut createdEmployer = employerService.create(employerDtoIn);
 
-        // Build the success response using the static method
-        ApiResponse<EmployerDtoOut> response = ApiResponse.success(createdEmployer);
+            // Build the success response using the static method
+            ApiResponse<EmployerDtoOut> response = ApiResponse.success(createdEmployer);
 
-        // Return the response
-        return ResponseEntity.status(HttpStatus.CREATED)
-                .body(response);
+            // Return the response
+            return ResponseEntity.status(HttpStatus.CREATED)
+                    .body(response);
+        } catch (ApiException e) {
+
+            // Build the error response using the static method
+            ApiResponse<EmployerDtoOut> response = ApiResponse.error(
+                    e.getErrorCode(),
+                    HttpStatus.CONFLICT,
+                    e.getMessage()
+            );
+
+            // Return the error response
+            return ResponseEntity.status(HttpStatus.CONFLICT)
+                    .body(response);
+        }
+
     }
 }
 
--- src/main/java/vn/unigap/api/service/EmployerServiceImpl.java
+++ src/main/java/vn/unigap/api/service/EmployerServiceImpl.java
@@ -1,14 +1,17 @@
 package vn.unigap.api.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import vn.unigap.api.common.ApiException;
+import vn.unigap.api.common.ErrorCode;
 import vn.unigap.api.dto.in.EmployerDtoIn;
 import vn.unigap.api.dto.out.EmployerDtoOut;
 import vn.unigap.api.entity.Employer;
 import vn.unigap.api.repository.EmployerRepository;
 
 @Service
-public class EmployerServiceImpl {
+public class EmployerServiceImpl implements EmployerService {
 
     private final EmployerRepository employerRepository;
 
@@ -17,7 +20,12 @@ public class EmployerServiceImpl {
         this.employerRepository = employerRepository;
     }
 
+    @Override
     public EmployerDtoOut create(EmployerDtoIn employerDtoIn) {
+        // Check if the email is existing yet
+        employerRepository.findByEmail(employerDtoIn.getEmail()).ifPresent(user -> {
+            throw new ApiException(ErrorCode.BAD_REQUEST, HttpStatus.BAD_REQUEST, "email already existed");
+        });
 
         Employer employer = employerRepository.save(Employer.builder()
                 .email(employerDtoIn.getEmail())
