b207e758133cd4b17e444cc7e37c11193738eebd
duke141 <vtminhduc140101@gmail.com>
2024-08-21 10:49:04 +0700

    Updated as feedbacks from previous commit

---
+++ src/main/java/vn/unigap/api/entity/Employer.java
@@ -0,0 +1,45 @@
+package vn.unigap.api.entity;
+
+import jakarta.persistence.*;
+
+import lombok.*;
+
+import java.time.LocalDateTime;
+import java.util.Date;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Table(name = "employer")
+public class Employer {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+
+    @Column(unique = true)
+    private String email;
+
+    private String name;
+
+    private Integer province;
+
+    private String description;
+
+    @Column(updatable = false)
+    private LocalDateTime created_at;
+
+    private LocalDateTime updated_at;
+
+    @PrePersist
+    protected void onCreate() {
+        created_at = LocalDateTime.now();
+        updated_at = LocalDateTime.now();
+    }
+
+    @PreUpdate
+    protected void onUpdate() {
+        updated_at = LocalDateTime.now();
+    }
+}
