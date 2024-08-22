52a7a91589a1260beab79736f3a73d0d2fb3b061
ducvotm <vtminhduc140101@gmail.com>
2024-08-22 09:39:40 +0700

    Update the filter in List method

--- src/main/java/vn/unigap/api/repository/JobRepository.java
+++ src/main/java/vn/unigap/api/repository/JobRepository.java
@@ -3,10 +3,14 @@ package vn.unigap.api.repository;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import vn.unigap.api.entity.Job;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface JobRepository extends JpaRepository<Job, Long> {
-    Page<Job> findAll(Pageable pageable);
+
+    @Query("SELECT j FROM Job j, Employer e WHERE j.employerId = e.id ORDER BY j.expiredAt DESC, e.name ASC")
+    Page<Job> findAllJobsOrderedByExpiredAtAndEmployerName(Pageable pageable);
 }
